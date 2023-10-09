9bbf8950d6c96b8fe820811b4a5b0a1219f530c4
WassiHaider <wassigujjar177@gmail.com>
2023-10-09 01:02:23 -0700

    added wassihaider component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Testing from './Testing'
+import wassihaider from './wassihaider'
 
 const App = () => {
   return (
     <div>
       welcome to webdevrs 
       <Testing />
+      <wassihaider/>
     </div>
   )
 }
---
+++ client/src/wassihaider.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function wassihaider (){
+    return( 
+        <H1>Wassi Haider</H1>
+    )
+}
+
+export default wassihaider
