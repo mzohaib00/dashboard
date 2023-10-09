4d5332dd223b89197641f0c3c0abd118d015a504
mzohaib00 <mzohaibiqbal00@gmail.com>
2023-10-09 13:02:03 +0500

    test

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Testing from './Testing'
+import Test from './Test'
 
 const App = () => {
   return (
     <div>
       welcome to webdevrs 
       <Testing />
+      <Test />
     </div>
   )
 }
